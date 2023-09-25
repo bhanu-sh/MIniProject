28cd19d4434e4b6534e716b119d03c4319f0bb74
Bhanu Sharma <bhanu1234sharma@gmail.com>
2023-09-25 18:52:04 +0530

    product

--- frontend/src/components/AddProduct.jsx
+++ frontend/src/components/AddProduct.jsx
@@ -4,10 +4,18 @@ const AddProduct = () => {
   return (
     <div className="container">
         <div className="">
-            <h1>Add Furniture:</h1>
-            <form className='' action="/addproduct" method="post">
-                <select name="" id="type">Enter</select>
-            </form>
+            <h1 className='text-center'>Add Furniture</h1>
+            <div className="card py-3 px-5 w-50 mx-auto">
+                <form className='' action="/addproduct" method="post">
+                    <label htmlFor="type">Type of Furniture:</label>
+                    <select className='form-control' id="type">
+                        <option value="sofa">Sofa</option>
+                        <option value="bed">Bed</option>
+                        <option value="table">Table</option>
+                        <option value="chair">Chair</option>
+                    </select>
+                </form>
+            </div>
         </div>
     </div>
   )
