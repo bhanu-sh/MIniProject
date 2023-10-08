84b1a4824e9440b059a4a96f0ec7766db8f7ac1f
Bhanu Sharma <bhanu1234sharma@gmail.com>
2023-10-08 20:24:51 +0530

    working profile page

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 import React, { useEffect } from "react";
 import { toast } from "react-hot-toast";
 import { useState } from "react";
@@ -5,9 +7,24 @@ import { Formik } from "formik";
 
 const Profile = () => {
   const id = JSON.parse(sessionStorage.user)._id;
+
+  const [userName, setUserName] = useState(
+    JSON.parse(sessionStorage.user).name
+  );
+  const [userEmail, setUserEmail] = useState(
+    JSON.parse(sessionStorage.user).email
+  );
+  const [userAvatar, setUserAvatar] = useState(
+    JSON.parse(sessionStorage.user).avatar
+  );
+
   console.log(id);
 
+  const [editInfo, setEditInfo] = useState(null);
+
   const [userData, setUserData] = useState(null);
+
+  const [toggleEdit, setToggleEdit] = useState(false);
 
   const fetchUserData = async () => {
     const res = await fetch(`http://localhost:5000/user/getbyid/${id}`);
@@ -37,6 +54,9 @@ const Profile = () => {
 
     if (res.status === 200) {
       toast.success("User Updated Successfully");
+      setUserEmail(values.email);
+      setUserName(values.name);
+      setToggleEdit(false);
     }
     setSubmitting(false);
   };
@@ -44,87 +64,126 @@ const Profile = () => {
   return (
     <div className="container">
       <h1 className="text-center text-decoration-underline">My Account</h1>
-      <h1>{id}</h1>
-
       <div className="card shadow">
-        <div className="card-body">
-          {userData !== null ? (
-            <Formik initialValues={userData} onSubmit={submitForm}>
-              {(signupForm) => (
-                <form onSubmit={signupForm.handleSubmit}>
-                  <h3 className="text-center">Edit Details:</h3>
-                  <hr />
-
-                  <label>Name</label>
-
-                  <span
-                    style={{ fontSize: "0.8em", color: "red", marginLeft: 20 }}
-                  >
-                    {signupForm.touched.name && signupForm.errors.name}
-                  </span>
-
-                  <input
-                    type="text"
-                    className="form-control mb-4"
-                    name="name"
-                    onChange={signupForm.handleChange}
-                    value={signupForm.values.name}
-                  />
-
-                  <label>Email</label>
-                  <span
-                    style={{ fontSize: "0.8em", color: "red", marginLeft: 20 }}
-                  >
-                    {signupForm.touched.email && signupForm.errors.email}
-                  </span>
-                  <input
-                    className="form-control mb-4"
-                    name="email"
-                    onChange={signupForm.handleChange}
-                    value={signupForm.values.email}
-                  />
-
-                  <label>Password</label>
-                  <span
-                    style={{ fontSize: "0.8em", color: "red", marginLeft: 20 }}
-                  >
-                    {signupForm.errors.password}
-                  </span>
-                  <input
-                    type="password"
-                    className="form-control mb-4"
-                    name="password"
-                    onChange={signupForm.handleChange}
-                    value={signupForm.values.password}
-                  />
-
-                  {/* <input type="file" onChange={uploadFile} /> */}
-
-                  <button
-                    disabled={signupForm.isSubmitting}
-                    type="submit"
-                    className="btn btn-primary mt-5 w-100"
-                  >
-                    {signupForm.isSubmitting ? (
-                      <>
-                        <span
-                          className="spinner-border spinner-border-sm"
-                          aria-hidden="true"
-                        ></span>
-                        <span>Loading ...</span>
-                      </>
-                    ) : (
-                      "Submit"
-                    )}
-                  </button>
-                </form>
-              )}
-            </Formik>
-          ) : (
-            <h1>Loading ... </h1>
-          )}
+        <div className="row">
+          <div className="col-md-2 ms-1 text-center">
+            <img
+              src={"http://localhost:5000/" + userAvatar}
+              width={200}
+              className="rounded-circle"
+              alt="Profile"
+            />
+          </div>
+          <div className="col-md-9 p-3 border-start">
+            <h5>Name: {userName}</h5>
+            <h5>E-Mail: {userEmail}</h5>
+            <button
+              className="btn btn-dark mt-3"
+              onClick={() => setToggleEdit(true)}
+            >
+              Edit Profile
+            </button>
+          </div>
         </div>
       </div>
+
+      {toggleEdit ? (
+        <>
+          <div className="edit-details mt-5">
+            <div className="card shadow">
+              <div className="card-body">
+                {userData !== null ? (
+                  <Formik initialValues={userData} onSubmit={submitForm}>
+                    {(signupForm) => (
+                      <form onSubmit={signupForm.handleSubmit}>
+                        <h3 className="text-center">Edit Details:</h3>
+                        <hr />
+
+                        <label>Name</label>
+
+                        <span
+                          style={{
+                            fontSize: "0.8em",
+                            color: "red",
+                            marginLeft: 20,
+                          }}
+                        >
+                          {signupForm.touched.name && signupForm.errors.name}
+                        </span>
+
+                        <input
+                          type="text"
+                          className="form-control mb-4"
+                          name="name"
+                          onChange={signupForm.handleChange}
+                          value={signupForm.values.name}
+                        />
+
+                        <label>Email</label>
+                        <span
+                          style={{
+                            fontSize: "0.8em",
+                            color: "red",
+                            marginLeft: 20,
+                          }}
+                        >
+                          {signupForm.touched.email && signupForm.errors.email}
+                        </span>
+                        <input
+                          className="form-control mb-4"
+                          name="email"
+                          onChange={signupForm.handleChange}
+                          value={signupForm.values.email}
+                        />
+
+                        <label>Password</label>
+                        <span
+                          style={{
+                            fontSize: "0.8em",
+                            color: "red",
+                            marginLeft: 20,
+                          }}
+                        >
+                          {signupForm.errors.password}
+                        </span>
+                        <input
+                          type="password"
+                          className="form-control mb-4"
+                          name="password"
+                          onChange={signupForm.handleChange}
+                          value={signupForm.values.password}
+                        />
+
+                        {/* <input type="file" onChange={uploadFile} /> */}
+
+                        <button
+                          disabled={signupForm.isSubmitting}
+                          type="submit"
+                          className="btn btn-primary mt-5 w-100"
+                        >
+                          {signupForm.isSubmitting ? (
+                            <>
+                              <span
+                                className="spinner-border spinner-border-sm"
+                                aria-hidden="true"
+                              ></span>
+                              <span>Loading ...</span>
+                            </>
+                          ) : (
+                            "Submit"
+                          )}
+                        </button>
+                      </form>
+                    )}
+                  </Formik>
+                ) : (
+                  <h1>Loading ... </h1>
+                )}
+              </div>
+            </div>
+          </div>
+        </>
+      ) : null}
     </div>
   );
 };
