3493223617d0d2fbe81d21d86a4fe1cbdd0838e6
bhanu-sh <bhanu1234sharma@gmail.com>
2024-05-17 21:47:09 +0530

    Updated CORS configuration for backend

--- backend/index.js
+++ backend/index.js
@@ -18,6 +18,8 @@ app.use(express.json());
 app.use(
   cors({
     origin: [process.env.FRONTEND_URL, "http://localhost:3000"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
   })
 );
 
